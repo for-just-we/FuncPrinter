bfee1ba31dc0ba6e63cbe018d54d5be15af1bb5c
prophe <1172782111@qq.com>
2025-05-19 18:18:07 +0800

    update analyze.py for extra arg

--- analyze.py
+++ analyze.py
@@ -10,6 +10,7 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", required=True, help="Path to build directory")
     parser.add_argument("-e", "--exe", required=False, default="FuncPrinter", help="executable path")
+    parser.add_argument("-extra", "--extra_arg", default="")
     return parser
 
 def main():
@@ -30,10 +31,14 @@ def main():
     file_names = ""
     for entry in data:
         file_name = entry.get("file")
+        if "directory" in entry.keys():
+            file_name = os.path.join(entry.get("directory"), file_name)
         if file_name:
             file_names += file_name + " "
 
     total_command = "{} -p {} {}".format(args.exe, build_path, file_names)
+    if args.extra != "":
+        total_command += " --extra-arg={}".format(args.extra)
     # 执行命令
     try:
         result = subprocess.run(total_command, shell=True, check=True, capture_output=True, text=True)
